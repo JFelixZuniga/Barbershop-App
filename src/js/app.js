bb4f3ecdaf74c3224682fada021a5d5df10ebd75
JFelixZuniga <johnfzuniga@gmail.com>
2021-02-12 23:07:38 -0300

    2 commit, DOM scripting y css

--- src/js/app.js
+++ src/js/app.js
@@ -9,13 +9,36 @@ function iniciarApp() {
 async function mostrarServicios() {
   try {
     const resultado = await fetch("./servicios.json");
-    const db = await resultado.json;
-    console.log(db);
+    const db = await resultado.json();
+    // Con la desestructuración extraemos el valor y creamos la variable en un solo paso
+    const { servicios } = db;
 
+    // Generamos el Html
+    servicios.forEach((servicio) => {
+      const { id, nombre, precio } = servicio;
 
-    const servicios = db.servicios;
+      // DOM Scripting
+      // Generamos el nombre del servicio
+      const nombreServicio = document.createElement("P");
+      nombreServicio.textContent = nombre;
+      nombreServicio.classList.add("nombre-servicio");
 
-    console.log(servicios);
+      // Generamos el precio del servicio
+      const precioServicio = document.createElement("P");
+      precioServicio.textContent = `$ ${precio}`;
+      precioServicio.classList.add("precio-servicio");
+
+      // Generamos el div contenedor del servicio
+      const servicioDiv = document.createElement("DIV");
+      servicioDiv.classList.add("servicio");
+
+      // Inyectamos precio y nombre en el div de servicio
+      servicioDiv.appendChild(nombreServicio);
+      servicioDiv.appendChild(precioServicio);
+
+      // Inyectamos el div en el Html
+      document.querySelector("#servicios").appendChild(servicioDiv);
+    });
   } catch (error) {
     console.log(error);
   }
